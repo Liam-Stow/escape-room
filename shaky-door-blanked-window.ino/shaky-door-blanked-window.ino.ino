c61456c5f888fd3e5fd5d13c6896686e8a63b916
Liam Stow <liam.stow@outlook.com>
2023-08-27 16:50:00 +1000

    pins defined

---
+++ shaky-door-blanked-window.ino/shaky-door-blanked-window.ino.ino
@@ -0,0 +1,14 @@
+constexpr int BUTTON_PIN = 1
+constexpr int SCREEN_RELAY_PIN = 2
+constexpr int DOOR_SHAKER_REPLAY_PIN = 2
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
